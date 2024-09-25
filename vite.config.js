a4ce86161b6afd028dacb7ec53964f835d2d548e
Phemmy <phemmy123x@gmail.com>
2024-09-25 14:02:41 +0100

    seventh  commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom', 'react-icons/fa']
-      
-    }
-  },
   plugins: [react()],
+  base: '/The-Light-Tutors-New-Website/'
 })
